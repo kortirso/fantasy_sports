94f7e4c54da18b1dca05a4b88d9976f044c69d57
Bogdanov Anton <kortirso@gmail.com>
2022-11-22 19:59:06 +0400

    fix request url

--- app/javascript/components/PlayerModal/requests/seasonPlayerRequest.tsx
+++ app/javascript/components/PlayerModal/requests/seasonPlayerRequest.tsx
@@ -2,7 +2,7 @@ import { apiRequest } from 'requests/helpers/apiRequest';
 
 export const seasonPlayerRequest = async (seasonUuid: string, playerUuid?: string) => {
   const result = await apiRequest({
-    url: `/seasons/${seasonUuid}/players/${playerUuid}.json?`,
+    url: `/seasons/${seasonUuid}/players/${playerUuid}.json`,
   });
   return result.season_player.data.attributes;
 };
