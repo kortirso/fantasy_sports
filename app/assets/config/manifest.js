cf3d20446d53af18089bfbd78fc4164da9ad896d
Bogdanov Anton <kortirso@gmail.com>
2023-10-04 21:59:26 +0300

    update manifest file

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,5 @@
-//= link emailbutler.css
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
+//= link emailbutler.css
 //= link_tree ../builds
