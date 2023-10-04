bde5b543d107020643074c21f5a4703c6e2a895c
Bogdanov Anton <kortirso@gmail.com>
2023-10-04 12:19:20 +0300

    refresh styles for admins

--- app/javascript/components/atoms/Flash.jsx
+++ app/javascript/components/atoms/Flash.jsx
@@ -16,7 +16,7 @@ export const Flash = ({ values }) => {
   return (
     <div className="fixed top-12 right-8">
       {Object.entries(messages).map(([key, value], index) => (
-        <div className={`mb-2 py-2 px-4 rounded-sm ${flashBackground(key)}`} key={index}>
+        <div className={`mb-2 py-2 px-4 rounded shadow ${flashBackground(key)}`} key={index}>
           <p>{value}</p>
         </div>
       ))}
