fe6475c2b3cda24feb0e58359385ae8353596214
Bogdanov Anton <kortirso@gmail.com>
2023-11-17 19:23:02 +0300

    add points page without lineup

--- app/controllers/fantasy_teams/points_controller.rb
+++ app/controllers/fantasy_teams/points_controller.rb
@@ -18,15 +18,14 @@ module FantasyTeams
     end
 
     def find_week
-      @week = @fantasy_team.weeks
-      @week = params[:week] ? @week.opponent_visible.find_by!(position: params[:week]) : @week.active.first
+      weeks = @fantasy_team.weeks
+      @week = params[:week] ? weeks.opponent_visible.find_by!(position: params[:week]) : weeks.active.first
+      @week = @fantasy_team.season.weeks.active.first if @week.nil?
     end
 
     def find_fantasy_team_relationships
-      return if @week.nil?
-
-      @lineup = Lineup.find_by!(fantasy_team: @fantasy_team, week: @week)
-      @season = @fantasy_team.season if @lineup
+      @lineup = Lineup.find_by(fantasy_team: @fantasy_team, week: @week)
+      @season = @fantasy_team.season
     end
   end
 end
--- app/javascript/components/SquadPoints/SquadPoints.jsx
+++ app/javascript/components/SquadPoints/SquadPoints.jsx
@@ -71,6 +71,7 @@ export const SquadPoints = ({
     return null;
   };
 
+
   return (
     <>
       <span className="badge-dark inline-block">
@@ -78,20 +79,26 @@ export const SquadPoints = ({
       </span>
       <h1 className="mb-2">{strings.squadPoints.title}</h1>
       <p className="mb-4">{strings.squadPoints.description}</p>
-      <div className="flex flex-col md:flex-row justify-between mt-2 bg-stone-200 border border-stone-300 rounded mb-4">
-        <div className="flex flex-row md:flex-col items-center justify-center md:justify-between flex-1 py-2 px-10 border-b md:border-b-0 md:border-r border-stone-300">
-          <p className="text-center">{strings.squadPoints.totalPoints}</p>
-          <p className="ml-4 md:ml-0 text-xl">{points}</p>
+      {lineupUuid ? (
+        <div className="flex flex-col md:flex-row justify-between mt-2 bg-stone-200 border border-stone-300 rounded mb-4">
+          <div className="flex flex-row md:flex-col items-center justify-center md:justify-between flex-1 py-2 px-10 border-b md:border-b-0 md:border-r border-stone-300">
+            <p className="text-center">{strings.squadPoints.totalPoints}</p>
+            <p className="ml-4 md:ml-0 text-xl">{points}</p>
+          </div>
+          <div className="flex flex-row md:flex-col items-center justify-center md:justify-between flex-1 py-2 px-10 border-b md:border-b-0 md:border-r border-stone-300">
+            <p className="text-center">{strings.squadPoints.averagePoints}</p>
+            <p className="ml-4 md:ml-0 text-xl">{averagePoints}</p>
+          </div>
+          <div className="flex flex-row md:flex-col items-center justify-center md:justify-between flex-1 py-2 px-10">
+            <p className="text-center">{strings.squadPoints.hightestPoints}</p>
+            <p className="ml-4 md:ml-0 text-xl">{maxPoints}</p>
+          </div>
         </div>
-        <div className="flex flex-row md:flex-col items-center justify-center md:justify-between flex-1 py-2 px-10 border-b md:border-b-0 md:border-r border-stone-300">
-          <p className="text-center">{strings.squadPoints.averagePoints}</p>
-          <p className="ml-4 md:ml-0 text-xl">{averagePoints}</p>
+      ) : (
+        <div>
+          <span className="badge-dark">{strings.squadPoints.noLineup}</span>
         </div>
-        <div className="flex flex-row md:flex-col items-center justify-center md:justify-between flex-1 py-2 px-10">
-          <p className="text-center">{strings.squadPoints.hightestPoints}</p>
-          <p className="ml-4 md:ml-0 text-xl">{maxPoints}</p>
-        </div>
-      </div>
+      )}
       <div className="flex justify-between items-center mb-4">
         <div>
           {previousPointsUrl ? (
@@ -109,48 +116,50 @@ export const SquadPoints = ({
           ) : null}
         </div>
       </div>
-      <div className={`${sportKind}-field`}>
-        <div className="flex flex-col relative bg-no-repeat bg-cover bg-center field">
-          {Object.entries(sportPositions).map(([positionKind, sportPosition]) => (
-            <div
-              className={`sport-position ${sportPositionName(sportPosition)}`}
-              key={positionKind}
-            >
-              {activePlayersByPosition(positionKind).map((item) => (
-                <PlayerCard
-                  key={item.uuid}
-                  teamName={pageState.teamNames[item.team.uuid]?.short_name}
-                  name={localizeValue(item.player.shirt_name)}
-                  value={item.points}
-                  number={item.teams_player.shirt_number}
-                  status={item.status}
-                  onInfoClick={() => setPlayerUuid(item.player.uuid)}
-                />
-              ))}
-            </div>
-          ))}
-        </div>
-        {sport.changes && (
-          <div className="changes">
-            <div className="flex flex-row justify-center items-center">
-              {activeChips.length > 0 ? (
-                <div className="badge-dark absolute top-2 left-2">{renderActiveChip()}</div>
-              ) : null}
-              {reservePlayers().map((item) => (
-                <PlayerCard
-                  key={item.uuid}
-                  teamName={pageState.teamNames[item.team.uuid]?.short_name}
-                  name={localizeValue(item.player.shirt_name)}
-                  value={item.points}
-                  number={item.teams_player.shirt_number}
-                  status={item.status}
-                  onInfoClick={() => setPlayerUuid(item.player.uuid)}
-                />
-              ))}
-            </div>
+      {lineupUuid ? (
+        <div className={`${sportKind}-field`}>
+          <div className="flex flex-col relative bg-no-repeat bg-cover bg-center field">
+            {Object.entries(sportPositions).map(([positionKind, sportPosition]) => (
+              <div
+                className={`sport-position ${sportPositionName(sportPosition)}`}
+                key={positionKind}
+              >
+                {activePlayersByPosition(positionKind).map((item) => (
+                  <PlayerCard
+                    key={item.uuid}
+                    teamName={pageState.teamNames[item.team.uuid]?.short_name}
+                    name={localizeValue(item.player.shirt_name)}
+                    value={item.points}
+                    number={item.teams_player.shirt_number}
+                    status={item.status}
+                    onInfoClick={() => setPlayerUuid(item.player.uuid)}
+                  />
+                ))}
+              </div>
+            ))}
           </div>
-        )}
-      </div>
+          {sport.changes && (
+            <div className="changes">
+              <div className="flex flex-row justify-center items-center">
+                {activeChips.length > 0 ? (
+                  <div className="badge-dark absolute top-2 left-2">{renderActiveChip()}</div>
+                ) : null}
+                {reservePlayers().map((item) => (
+                  <PlayerCard
+                    key={item.uuid}
+                    teamName={pageState.teamNames[item.team.uuid]?.short_name}
+                    name={localizeValue(item.player.shirt_name)}
+                    value={item.points}
+                    number={item.teams_player.shirt_number}
+                    status={item.status}
+                    onInfoClick={() => setPlayerUuid(item.player.uuid)}
+                  />
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
+      ) : null}
       {Object.keys(pageState.teamNames).length > 0 ? (
         <Week uuid={weekUuid} teamNames={pageState.teamNames} />
       ) : null}
