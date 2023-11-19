26d40f38c50374046f95a93843d6650a38bbda16
Bogdanov Anton <kortirso@gmail.com>
2023-11-19 20:27:55 +0300

    add watchlist for pages of other fantasy teams

--- app/controllers/concerns/watchable.rb
+++ app/controllers/concerns/watchable.rb
@@ -8,8 +8,14 @@ module Watchable
   def set_watchable_players
     return if Current.user.blank?
     return if @fantasy_team.nil?
-    return unless @fantasy_team.user_id == Current.user.id
 
-    @watches = @fantasy_team.fantasy_teams_watches.joins(:players_season).pluck('players_seasons.uuid')
+    user_fantasy_team =
+      if @fantasy_team.user_id == Current.user.id
+        @fantasy_team
+      else
+        FantasyTeam.joins(:season).find_by(seasons: { id: @fantasy_team.season_id })
+      end
+
+    @watches = user_fantasy_team.fantasy_teams_watches.joins(:players_season).pluck('players_seasons.uuid')
   end
 end
