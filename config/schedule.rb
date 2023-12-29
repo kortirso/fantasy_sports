7c30dc4c1fa2f734ad21e7d6a44157fc95201817
Bogdanov Anton <kortirso@gmail.com>
2023-12-29 21:01:59 +0300

    change schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,6 +6,7 @@
 # At minute 0 and 30
 every '0,30 * * * *' do
   runner 'SchedulerJob.perform_later'
+  runner 'Deliveries::User::DeadlineReportJob.perform_later'
 end
 
 # At minute 0 past hour 0 and 12
@@ -17,8 +18,4 @@ end
 every 1.day do
   runner 'Users::Sessions::RemoveExpiredJob.perform_later'
   runner 'Injuries::RemoveExpiredJob.perform_later'
-end
-
-every 1.hour do
-  runner 'Deliveries::User::DeadlineReportJob.perform_later'
 end
