4defb259ad21ad5b0c914edea297b281286cdc20
Bogdanov Anton <kortirso@gmail.com>
2024-02-28 11:10:02 +0300

    fixed bug with rendering oracul page without lineup

--- app/javascript/components/OraculWeek/requests/forecastsRequest.jsx
+++ app/javascript/components/OraculWeek/requests/forecastsRequest.jsx
@@ -1,6 +1,8 @@
 import { apiRequest } from '../../../requests/helpers/apiRequest';
 
 export const forecastsRequest = async (uuid) => {
+  if (uuid === null) return [];
+
   const result = await apiRequest({ url: `/api/frontend/oraculs/forecasts.json?oraculs_lineup_id=${uuid}`});
   return result.forecasts.data.map((element) => element.attributes);
 };
